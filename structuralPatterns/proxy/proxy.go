dfc9ecc3129df2553f94da5141f641184f68e6d1
Farber98 <juantuc98@gmail.com>
2022-03-03 12:43:04 -0300

    fix: proxy pattern

--- structuralPatterns/proxy/proxy.go
+++ structuralPatterns/proxy/proxy.go
@@ -1,42 +1,78 @@
 package proxy
 
-import "errors"
-
-/*
-PROXY:
-- Wraps an object to hide some of its characteristics (remote object, very heavy object, restricted access object)
-
-OBJECTIVE:
-- Hide an object behind the proxy so the features can be hidden , restricted and so on.
-- Provide a new abstraction layer that is easy to work with, and can be changed easily
-
-EXAMPLE: Remote proxy going to be a FIFO cache of objects before accessing DB.
-
-ACCEPTANCE CRITERIA:
-- All access to the DB os users will be done through the Proxy type.
-- A stack of n number of recent users will be kept in the Proxy.
-- If a user alread exists in the stack, it won't query the database and will return the stored one.
-- If the queried user doesn't exist in the stack, it will query the database, remove the oldest user in the stack if it's full, store the new one, and return it.
-
-*/
-
-type UserFinder interface {
-	FindUser(id int32) (User, error)
-}
+import "fmt"
 
 type User struct {
 	ID int32
 }
 
+type UserFinder interface {
+	FindUser(id string) (User, error)
+}
+
+// ----------------------------------------------------------------------
+
 type UserList []User
 
+//FindUser will iterate over the list to try to find a user with the same
+//name that the param or return an error if it can't find it
+func (t *UserList) FindUser(id int32) (User, error) {
+	for i := 0; i < len(*t); i++ {
+		if (*t)[i].ID == id {
+			return (*t)[i], nil
+		}
+	}
+
+	return User{}, fmt.Errorf("User %d could not be found\n", id)
+}
+
+//AddUser adds a new user to the end of the Users slice
+func (t *UserList) addUser(newUser User) {
+	*t = append(*t, newUser)
+}
+
+// ----------------------------------------------------------------------
+
 type UserListProxy struct {
-	SomeDatabase           UserList
+	MockedDatabase         *UserList
 	StackCache             UserList
-	StackCapacity          int
+	StackSize              int
 	DidLastSearchUsedCache bool
 }
 
+//addUserToStack takes the user argument and adds it to the stack in place.
+//If the stack is full it removes the first element on it before adding.
+func (u *UserListProxy) addUserToStack(user User) {
+	if len(u.StackCache) >= u.StackSize {
+		u.StackCache = append(u.StackCache[1:], user)
+	} else {
+		u.StackCache.addUser(user)
+	}
+}
+
+//FindUser will search for the specified name in the parameter in the cache
+//list. If it finds it, it will return it. If not, it will search in the heavy
+//list. Finally, if it's not in the heavy list, it will return an error
+//(generated from the heavy list)
 func (u *UserListProxy) FindUser(id int32) (User, error) {
-	return User{}, errors.New("not implemented yet")
+	//Search for the object in the cache list first
+	user, err := u.StackCache.FindUser(id)
+	if err == nil {
+		fmt.Println("Returning user from cache")
+		u.DidLastSearchUsedCache = true
+		return user, nil
+	}
+
+	//Object is not in the cache list. Search in the heavy list
+	user, err = u.MockedDatabase.FindUser(id)
+	if err != nil {
+		return User{}, err
+	}
+
+	//Adds the new user to the stack, removing the last if necessary
+	u.addUserToStack(user)
+
+	fmt.Println("Returning user from database")
+	u.DidLastSearchUsedCache = false
+	return user, nil
 }
