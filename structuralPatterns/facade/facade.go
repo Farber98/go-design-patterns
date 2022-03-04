68cc00af4bd6e9f394af71905feb157d684ca1ff
Farber98 <juantuc98@gmail.com>
2022-03-04 14:05:19 -0300

    feat: facade pattern

--- structuralPatterns/facade/facade.go
+++ structuralPatterns/facade/facade.go
@@ -1,13 +1,136 @@
 package facade
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
 /*
 FACADE:
+- A Facade is the front wall that hides rooms and corridors of a building. It protects its inhabitants and provides pryvacy. It orders and divides the dwellings.
+- This desing pattern does the same. It shields the code from unwanted access, orders some calls and hides the complexity scope from the user.
 
 OBJECTIVE:
+- Use Facade when you want to hide the complexiti of some tasks, especially when most of them share utilities (eg. auth in API).
+- A library is a form of facade, where someone has to provide some methods for a developer to do certain things in a friendly way.
+- If a developer needs to use your library, he doesn't need to know all the inner tasks to retrieve the result he wants.
 
 SITUATIONS:
+- You want to decrease the complexity of some parts of our code. You hide that complexity behinde the facade by providing a more easy-to-use method.
+- When you want to group actions that are cross-related in a single place.
+- When you want to build a library so that others can use your products without worrying about how it all works.
 
-EXAMPLE:
+EXAMPLE: Write a library that accesess OpenWeatherMaps service.
 
 ACCEPTANCE CRITERIA:
+- Provide a single type to access the data. All info retrieved from OpenWeatherMap service will pass through it.
+- Create a way to get the weather data for some city of some country
+- Creatte a way to get the weather data for some latitud and longitude position
+- Only second and third point must be visible outside of the package; everything else must be hidden (including all conection-related data).
+
 */
+
+const (
+	commonRequestPrefix              = "http://api.openweathermap.org/data/2.5/"
+	weatherByCityName                = commonRequestPrefix + "weather?q=%s,%s&APPID=%s"
+	weatherByGeographicalCoordinates = commonRequestPrefix + "weather?lat=%f&lon=%f&APPID=%s"
+)
+
+type CurrentWeatherDataRetriever interface {
+	GetByCityAndCountryCode(ctiy, countryCode string) (Weather, error)
+	GetByGeoCoordinates(lat, lon float32) (Weather, error)
+}
+
+type CurrentWeatherData struct {
+	APIKey string
+}
+
+type Weather struct {
+	Coord struct {
+		Lon float32 `json:"lon"`
+		Lat float32 `json:"lat"`
+	} `json:"coord"`
+	Weather []struct {
+		Id          int    `json:"id"`
+		Main        string `json:"main"`
+		Description string `json:"description"`
+		Icon        string `json:"icon"`
+	} `json:"weather"`
+	Base string `json:"base"`
+	Main struct {
+		Temp     float32 `json:"temp"`
+		Pressure float32 `json:"pressure"`
+		Humidity float32 `json:"humidity"`
+		TempMin  float32 `json:"temp_min"`
+		TempMax  float32 `json:"temp_max"`
+	} `json:"main"`
+	Wind struct {
+		Speed float32 `json:"speed"`
+		Deg   float32 `json:"deg"`
+	} `json:"wind"`
+	Clouds struct {
+		All int `json:"all"`
+	} `json:"clouds"`
+	Rain struct {
+		ThreeHours float32 `json:"3h"`
+	} `json:"rain"`
+	Dt  uint32 `json:"dt"`
+	Sys struct {
+		Type    int     `json:"type"`
+		ID      int     `json:"id"`
+		Message float32 `json:"message"`
+		Country string  `json:"country"`
+		Sunrise int     `json:"sunrise"`
+		Sunset  int     `json:"sunset"`
+	} `json:"sys"`
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	Cod  int    `json:"cod"`
+}
+
+func (c *CurrentWeatherData) responseParser(body io.Reader) (*Weather, error) {
+	w := new(Weather)
+	err := json.NewDecoder(body).Decode(w)
+	if err != nil {
+		return nil, err
+	}
+	return w, nil
+}
+
+func (c *CurrentWeatherData) GetByGeoCoordinates(lat, lon float32) (weather *Weather, err error) {
+	return c.doRequest(fmt.Sprintf(weatherByGeographicalCoordinates, lat, lon, c.APIKey))
+}
+
+func (c *CurrentWeatherData) GetByCityAndCountryCode(city, countryCode string) (weather *Weather, err error) {
+	return c.doRequest(fmt.Sprintf(weatherByCityName, city, countryCode, c.APIKey))
+
+}
+
+func (o *CurrentWeatherData) doRequest(uri string) (weather *Weather, err error) {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", uri, nil)
+	if err != nil {
+		return
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return
+	}
+
+	if resp.StatusCode != 200 {
+		byt, errMsg := ioutil.ReadAll(resp.Body)
+		if errMsg == nil {
+			errMsg = fmt.Errorf("%s", string(byt))
+		}
+		err = fmt.Errorf("Status code was %d, aborting. Error message was \n%s\n", resp.StatusCode, errMsg)
+		return
+	}
+	weather, err = o.responseParser(resp.Body)
+	resp.Body.Close()
+	return
+}
