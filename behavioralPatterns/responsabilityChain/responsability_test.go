1645649faafbcd77153af75d2964dac0d2d8afb8
Farber98 <juantuc98@gmail.com>
2022-03-06 14:18:16 -0300

    feat: responsability chain pattern

--- behavioralPatterns/responsabilityChain/responsability_test.go
+++ behavioralPatterns/responsabilityChain/responsability_test.go
@@ -1,6 +1,7 @@
 package responsability
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 )
@@ -39,4 +40,21 @@ func TestCreateDefaultChain(t *testing.T) {
 				t.Fatal("Last link didn't received expected message")
 			}
 		})
+	t.Run("2 loggers, second uses the closure implementation", func(t *testing.T) {
+		myWriter = myTestWriter{}
+		closureLogger := ClosureChain{
+			Closure: func(s string) {
+				fmt.Printf("My closure logger! Message: %s\n", s)
+				myWriter.receivedMessage = &s
+			},
+		}
+
+		WriterLogger.NextChain = &closureLogger
+
+		chain.Next("Hello closure logger")
+
+		if *myWriter.receivedMessage != "Hello closure logger" {
+			t.Fatal("Expected message wasn't received in myWriter")
+		}
+	})
 }
