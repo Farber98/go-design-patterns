ec4d457d93c9c6edb7222be804c875c4d3d2b5de
Farber98 <juantuc98@gmail.com>
2022-03-06 14:32:58 -0300

    feat: command pattern structure

---
+++ behavioralPatterns/command/command.go
@@ -0,0 +1,35 @@
+package command
+
+import "fmt"
+
+/*
+COMMAND:
+- When you need to connect types that are really unrelated.
+
+OBJECTIVE:
+- We are trying to encapsulate some sort of action or information in a light package that must be processed somewhere else.
+- Put some information into a box. Just the receiver will open the box and know its contents.
+- Delegate some action somewhere else.
+
+EXAMPLE: Simple queue.
+- We will put some information into a command implementer and we will have a queue.
+- We will create many instances of a type implementing a Command pattern and we will pass them to a queue that will store the commands until three of them are in the queue, at which time it will process them.
+
+
+ACCEPTANCE CRITERIA:
+- Command should reflect somehow the creation of a box that can accept urelated types and the execution of the Command itself.
+- We need a constructor of console printing commands. When we using this constructor with a string, it will return a command that will print it. In this case, the handler inside is the command that acts as a bos and as a handler.
+- We need a data structure that stores incoming commands in a queue and prints them once the queue reaches the length of three.
+*/
+
+type Command interface {
+	Execute()
+}
+
+type ConsoleOutput struct {
+	Message string
+}
+
+func (c *ConsoleOutput) Execute() {
+	fmt.Println(c.Message)
+}
