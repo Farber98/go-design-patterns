5d505a417cec9614a2e3b441f3cca88152152361
Farber98 <juantuc98@gmail.com>
2022-03-06 17:31:13 -0300

    feat: examples from command pattern

--- behavioralPatterns/command/ex3/command.go
+++ behavioralPatterns/command/ex3/command.go
@@ -1,1 +1,42 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Command interface {
+	Info() string
+}
+
+type TimePassed struct {
+	Start time.Time
+}
+
+func (t *TimePassed) Info() string {
+	return time.Since(t.Start).String()
+}
+
+type ChainLogger interface {
+	Next(Command)
+}
+
+type Logger struct {
+	NextChain ChainLogger
+}
+
+func (f *Logger) Next(c Command) {
+	time.Sleep(time.Second)
+	fmt.Printf("Elapsed time from creation: %s\n", c.Info())
+
+	if f.NextChain != nil {
+		f.NextChain.Next(c)
+	}
+}
+
+func main() {
+	second := new(Logger)
+	first := Logger{second}
+	command := &TimePassed{time.Now()}
+	first.Next(command)
+}
