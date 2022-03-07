b8b01941fe7dda86451d88166882acc473dccd7c
Farber98 <juantuc98@gmail.com>
2022-03-07 08:39:58 -0300

    feat: memento pattern structure

---
+++ behavioralPatterns/memento/memento.go
@@ -0,0 +1,58 @@
+package memento
+
+import "fmt"
+
+/*
+MEMENTO:
+- We'll have a type with some state and we want to be able to save milestons of its state.
+- Having a finite amount of states saved, we can recoverr them if necesary for a variety of tasks-undo operations, historics, and so on.
+- Usually has 3 actors:
+	* Memento: A type that stores the type we want to save. Usually, we wont store the business type directly and we provide an extra layer of abstraction through this type.
+	* Originator: A type that is in charge of creating mementos and storing the current active state. We said that the Memento type wraps states of the business type and we use originator as the creator of mementos.
+	* Care taker: A type that stores the list of mementos that can have the logic to store them in a database or to not store more than specified number of them
+
+OBJECTIVE:
+- Memento is all about a sequence of actions over time, used to:
+	* Capture an object state without modifying the object itself
+	* Save limited amount of states so we can retrieve them later.
+
+EXAMPLE: strings.
+- Use a string as the state we want to save.
+- The string stored in a field of a State instance, will be modified and we will be able to undo the operations done in this state.
+
+ACCEPTANCE CRITERIA:
+- We need to store a finite amount of states of type string
+- We need a way to restore the current stored state to one of the state list.
+*/
+
+type State struct {
+	Description string
+}
+
+type Memento struct {
+	State State
+}
+
+type Originator struct {
+	State State
+}
+
+func (o *Originator) NewMemento() Memento {
+	return Memento{}
+}
+
+func (o *Originator) ExtractAndStoreState(m Memento) {
+
+}
+
+type CareTaker struct {
+	MementoList []Memento
+}
+
+func (c *CareTaker) Add(m Memento) {
+
+}
+
+func (c *CareTaker) Memento(i int) (Memento, error) {
+	return Memento{}, fmt.Errorf("not implemented yet")
+}
---
+++ behavioralPatterns/memento/memento_test.go
@@ -0,0 +1,38 @@
+package memento
+
+import "testing"
+
+func TestCareTaker_Add(t *testing.T) {
+	originator := Originator{State{Description: "Idle"}}
+	CareTaker := CareTaker{}
+	mem := originator.NewMemento()
+	if mem.State.Description != "Idle" {
+		t.Error("Expected state was not found")
+	}
+	currentLen := len(CareTaker.MementoList)
+	CareTaker.Add(mem)
+	if len(CareTaker.MementoList) != currentLen+1 {
+		t.Error("no new elements were added on the list")
+	}
+}
+
+func TestCareTaker_Memento(t *testing.T) {
+	originator := Originator{State{Description: "Idle"}}
+	CareTaker := CareTaker{}
+	CareTaker.Add(originator.NewMemento())
+	mem, err := CareTaker.Memento(0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if mem.State.Description != "Idle" {
+		t.Error("Unexpected state")
+	}
+	mem, err = CareTaker.Memento(-1)
+	if err != nil {
+		t.Fatal("An error is expected when asking for a negative number but no error was found")
+	}
+}
+
+func TestOriginator_ExtractAndStoreState(t *testing.T) {
+
+}
